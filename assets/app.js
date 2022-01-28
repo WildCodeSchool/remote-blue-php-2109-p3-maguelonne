2fa0ab3752836bf4953f8976f7d03c69ce94d67e
Jean-François Morin <jeanfrancois.morin@wildcodeschool.com>
2022-01-28 18:00:15 +0100

    fix scroll effect

--- assets/app.js
+++ assets/app.js
@@ -13,7 +13,7 @@ import './bootstrap';
 
 window.bootstrap = require('bootstrap');
 
-const btn = document.querySelector('#btn-scroll');
+const btn = document.getElementById('btn-scroll');
 
 btn.addEventListener('click', () => {
     window.scrollTo({
@@ -21,11 +21,12 @@ btn.addEventListener('click', () => {
         left: 0,
         behavior: 'smooth',
     });
-    window.onscroll = () => {
-        if (window.scrollY > 350) {
-            btn.style.opacity = 1;
-        } else {
-            btn.style.opacity = 0;
-        }
-    };
 });
+
+window.onscroll = () => {
+    if (window.scrollY > 350) {
+        btn.classList.add('show');
+    } else {
+        btn.classList.remove('show');
+    }
+};
