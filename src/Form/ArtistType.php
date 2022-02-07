373ffcde596641ad7fa883d4ca70cd4f99526f3a
emmanuelle <emmanuelle@orange.fr>
2022-02-07 21:02:00 +0100

    ajout dans la liste de nouveau instruments

--- src/Form/ArtistType.php
+++ src/Form/ArtistType.php
@@ -11,7 +11,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use FOS\CKEditorBundle\Form\Type\CKEditorType;
 
 class ArtistType extends AbstractType
 {
@@ -28,9 +27,28 @@ class ArtistType extends AbstractType
             ->add('instruments', ChoiceType::class, [
                 'choices' => [
                     'chant' => 'voice',
-                    'Violoncelle' => 'cello',
+                    'violoncelle' => 'cello',
                     'violon' => 'violin',
                     'piano' => 'piano',
+                    'alto' => 'viola',
+                    'contrebasses' => 'bass',
+                    'harpe' => 'harp',
+                    'piccolo' => 'piccolo',
+                    'flûte' => 'flute',
+                    'hautbois' => 'oboe',
+                    'cor anglais' => 'english horn',
+                    'clarinette' => 'clarinet',
+                    'clarinette basse' => 'bass clarinet',
+                    'basson' => 'bassoon',
+                    'contrebasson' => 'contrabassoon',
+                    'cor' => ' french horn',
+                    'trompette' => 'trumpet',
+                    'trombone' => 'trombone',
+                    'tuba' => 'tuba',
+                    'percussion' => 'percussion',
+                    'timbales' => 'timpani',
+                    'chef d\'orchestre' => 'constructor',
+                    'autre' => 'other',
                 ],
                 'multiple' => true,
                 'expanded' => true,
@@ -49,9 +67,8 @@ class ArtistType extends AbstractType
                 'required' => false,
                 'label' => 'lien audio',
             ])
-            ->add('body', CKEditorType::class, [
+            ->add('body', TextareaType::class, [
                 'label' => 'Biographie de l\' artiste',
-                'config_name' => 'full',
                 'attr' => ['rows' => 10],
             ])
             ->add('alt', TextType::class, [
