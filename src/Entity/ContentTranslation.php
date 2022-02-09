1d26f5d9c69605f7f565939fd886fce6bf05e992
Geoffrey Vercaemer <geoffrey.vercaemer@gmail.com>
2022-02-09 18:44:52 +0100

    fix

--- src/Entity/ContentTranslation.php
+++ src/Entity/ContentTranslation.php
@@ -13,69 +13,69 @@ use Knp\DoctrineBehaviors\Contract\Entity\TranslationInterface;
  */
 class ContentTranslation implements TranslationInterface
 {
-    use TranslationTrait;
+  use TranslationTrait;
 
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private int $id;
+  /**
+   * @ORM\Id
+   * @ORM\GeneratedValue
+   * @ORM\Column(type="integer")
+   */
+  private int $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     * @Assert\NotBlank(message="Merci de remplir ce champ")
-     */
-    private string $title = "";
+  /**
+   * @ORM\Column(type="string", length=255)
+   * @Assert\NotBlank(message="Merci de remplir ce champ")
+   */
+  private string $title = "";
 
-    /**
-     * @ORM\Column(type="text")
-     */
-    private string $body = "";
+  /**
+   * @ORM\Column(type="text")
+   */
+  private string $body = "";
 
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private ?string $alt = "";
+  /**
+   * @ORM\Column(type="string", length=255, nullable=true)
+   */
+  private ?string $alt = "";
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
 
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
+  public function getTitle(): ?string
+  {
+    return $this->title;
+  }
 
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
+  public function setTitle(string $title): self
+  {
+    $this->title = $title;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getBody(): ?string
-    {
-        return $this->body;
-    }
+  public function getBody(): ?string
+  {
+    return $this->body;
+  }
 
-    public function setBody(string $body): self
-    {
-        $this->body = $body;
+  public function setBody(string $body): self
+  {
+    $this->body = $body;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getAlt(): ?string
-    {
-        return $this->alt;
-    }
+  public function getAlt(): ?string
+  {
+    return $this->alt;
+  }
 
-    public function setAlt(?string $alt): self
-    {
-        $this->alt = $alt;
+  public function setAlt(?string $alt): self
+  {
+    $this->alt = $alt;
 
-        return $this;
-    }
+    return $this;
+  }
 }
