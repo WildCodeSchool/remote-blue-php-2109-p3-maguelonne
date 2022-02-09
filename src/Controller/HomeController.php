7970114812180cf825d1d9752e993863aa14ee0f
Geoffrey Vercaemer <geoffrey.vercaemer@gmail.com>
2022-02-09 15:15:45 +0100

    Suppression fichiers obsolètes Admin + Redirect

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,8 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Content;
-use App\Repository\ContentRepository;
 
 class HomeController extends AbstractController
 {
@@ -16,20 +14,5 @@ class HomeController extends AbstractController
     public function indexNoLocale(): Response
     {
         return $this->redirectToRoute('home');
-    }
-
-    /**
-     * @Route("/{_locale}", name="home")
-     */
-    public function index(ContentRepository $contentRepository): Response
-    {
-        return $this->render(
-            'home/index.html.twig',
-            [
-                'content' => $contentRepository->findOneBy([
-                    'id' => 1,
-                ])
-            ]
-        );
     }
 }
